e43d3f1c6208ae485ae913ad96b1f4bba98c2630
poco.ye <poco.ye@rock-chips.com>
2019-06-06 05:00:19 +0200

    3368 6.0 bluetooth

---
+++ device/common/bluetooth/libbt-rtk/uart/Android.mk
@@ -0,0 +1,26 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+BDROID_DIR := $(TOP_DIR)system/bt
+
+LOCAL_SRC_FILES := \
+        src/bt_vendor_rtk.c \
+        src/hardware.c \
+        src/userial_vendor.c \
+        src/upio.c
+
+LOCAL_C_INCLUDES += \
+        $(LOCAL_PATH)/include \
+        $(BDROID_DIR)/hci/include \
+        $(BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR)/uart
+
+LOCAL_SHARED_LIBRARIES := \
+        libcutils \
+        libhardware_legacy
+
+LOCAL_MODULE := libbt-vendor_uart
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)
